b02a5c8d9fb01a80b96596ceb00a6081a29a13ba
Daniel Schötz <schoetz@phooky.com>
2022-10-31 13:26:38 +0100

    Update urls.py

--- rest_framework_social_oauth2/urls.py
+++ rest_framework_social_oauth2/urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, re_path
+from django.urls import include, re_path, path
 from oauth2_provider.views import AuthorizationView
 
 from .views import ConvertTokenView, TokenView, RevokeTokenView, invalidate_sessions, DisconnectBackendView
@@ -8,7 +8,7 @@ app_name = 'drfso2'
 urlpatterns = [
     re_path(r'^authorize/?$', AuthorizationView.as_view(), name="authorize"),
     re_path(r'^token/?$', TokenView.as_view(), name="token"),
-    re_path('', include('social_django.urls', namespace="social")),
+    path('', include('social_django.urls', namespace="social")),
     re_path(r'^convert-token/?$', ConvertTokenView.as_view(), name="convert_token"),
     re_path(r'^revoke-token/?$', RevokeTokenView.as_view(), name="revoke_token"),
     re_path(r'^invalidate-sessions/?$', invalidate_sessions, name="invalidate_sessions"),
